b5235c6b7336ccb6fe15108fd49d66676925cb2b
Michael <yang.5915@buckeyemail.osu.edu>
2022-11-04 10:11:25 -0400

    Add some elements for sound

---
+++ sprint0/Sound/ISound.cs
@@ -0,0 +1,55 @@
+﻿
+
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections;
+using System.ComponentModel;
+using System.Reflection.Metadata;
+using Microsoft.Xna.Framework.Input;
+using System.Runtime.InteropServices;
+using static System.Formats.Asn1.AsnWriter;
+using static sprint0.Link;
+
+
+namespace sprint0
+{
+    public interface ISound
+    {
+
+
+
+
+
+
+    }
+
+
+
+
+    public class Sound : ISound
+    {
+
+        private SoundEffect soundEffect;
+        public Sound(SoundEffect soundEffect)
+        {
+            this.soundEffect = soundEffect;
+        }
+
+        public void Play()
+        {
+
+            soundEffect.play();
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+}
